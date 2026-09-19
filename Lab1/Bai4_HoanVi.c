240fbb0625e53cadc999044539c31bd2c8533c0b
srk-279 <tkhaikhait@gmail.com>
2026-09-19 08:26:12 +0700

    Bai4

---
+++ Lab1/Bai4_HoanVi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 6;
+    int b = 7;
+
+    printf("Truoc khi hoan vi: a = %d, b = %d\n", a, b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("Sau khi hoan vi: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
